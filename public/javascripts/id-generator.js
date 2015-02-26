a5f238ea6d45fa009f413cb03300155027237951
Noah Paessel <knowuh@gmail.com>
2015-02-26 12:58:55 -0500

    Ignore the /public/ folder (except for config.js)

--- public/javascripts/id-generator.js
+++
@@ -1,9 +0,0 @@
-var ids = {};
-
-var idgen = function(scopeName) {
-  ids[scopeName] = ids[scopeName] || 0;
-  ids[scopeName] = ids[scopeName] + 1;
-  return scopeName + "-" + ids[scopeName];
-};
-
-module.exports = idgen;
