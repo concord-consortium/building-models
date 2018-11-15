a6e6ca3941e31745f8fc430329b0242f73d5286e
Doug Martin <doug@zoopdoop.com>
2018-11-15 09:48:18 -0500

    Fixed valve icon not coming up [#161824729]

--- src/code/models/transfer.ts
+++ src/code/models/transfer.ts
@@ -12,6 +12,9 @@ const DEFAULT_COMBINE_METHOD = "product";
 
 export class TransferModel extends Node {
   public type = "Transfer";
+  public isTransfer = true;
+  public combineMethod = DEFAULT_COMBINE_METHOD;
+
   protected transferLink: any; // TODO: get concrete type
 
   public setTransferLink(link) {
