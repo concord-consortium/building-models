f02f24087b9954345361b2b470f1bdd0153dfd01
pjanik <janikpiotrek@gmail.com>
2018-11-23 17:01:02 +0100

    Refactor migration tests, add test of the new migration
    [#162022659]

--- test/migrations-test.ts
+++ test/migrations-test.ts
@@ -13,27 +13,27 @@ chai.config.includeStack = true;
 
 const should = chai.should();
 
-export {};
-
 describe("Migrations",  () =>
   describe("update", () => {
+    let data;
     beforeEach(() => {
-      this.result = migrationUpdate(v01data);
+      data = _.cloneDeep(v01data);
     });
 
     describe("the final version number", () =>
-      it("should be 1.24.0", () => {
-        this.result.version.should.equal("1.24.0");
+      it("should be 1.25.0", () => {
+        migrationUpdate(data).version.should.equal("1.25.0");
       })
     );
 
     describe("the nodes", () => {
       it("should have two nodes", () => {
-        this.result.nodes.length.should.equal(2);
+        migrationUpdate(data).nodes.length.should.equal(2);
       });
 
       it("should have key: and data: attributes", () => {
-        for (const node of this.result.nodes) {
+        const result = migrationUpdate(data);
+        for (const node of result.nodes) {
           node.key.should.not.equal(undefined);
           node.data.should.not.equal(undefined);
         }
@@ -42,23 +42,25 @@ describe("Migrations",  () =>
 
     describe("the palette", () => {
       it("should exist", () => {
-        this.result.palette.should.not.equal(undefined);
+        migrationUpdate(data).palette.should.not.equal(undefined);
       });
       it("should include a blank node", () => {
-        const blank = _.find(this.result.palette, pitem => pitem.key === "img/nodes/blank.png");
+        const result = migrationUpdate(data);
+        const blank = _.find(result.palette, pitem => pitem.key === "img/nodes/blank.png");
         blank.should.not.equal(undefined);
       });
     });
 
     describe("the links", () =>
       it("should have one link", () => {
-        this.result.links.length.should.equal(1);
+        migrationUpdate(data).links.length.should.equal(1);
       })
     );
 
     describe("v-1.1 changes", () => {
       it("should have initial node values", () => {
-        for (const node of this.result.nodes) {
+        const result = migrationUpdate(data);
+        for (const node of result.nodes) {
           node.data.initialValue.should.not.equal(undefined);
           node.data.isAccumulator.should.not.equal(undefined);
           node.data.isAccumulator.should.equal(false);
@@ -66,7 +68,8 @@ describe("Migrations",  () =>
       });
 
       it("should have valid relationship values", () => {
-        for (const link of this.result.links) {
+        const result = migrationUpdate(data);
+        for (const link of result.links) {
           link.relation.should.not.equal(undefined);
           // WAS expect(link.relation.text).to.be.undefined();
           expect(link.relation.text).to.equal(undefined);
@@ -78,7 +81,8 @@ describe("Migrations",  () =>
 
     describe("v-1.2 node changes", () =>
       it("should have initial node values", () => {
-        for (const node of this.result.nodes) {
+        const result = migrationUpdate(data, "1.2.0");
+        for (const node of result.nodes) {
           node.data.valueDefinedSemiQuantitatively.should.not.equal(undefined);
           node.data.valueDefinedSemiQuantitatively.should.equal(true);
         }
@@ -87,7 +91,8 @@ describe("Migrations",  () =>
 
     describe("v-1.3 node changes", () =>
       it("should have initial node values", () => {
-        for (const node of this.result.nodes) {
+        const result = migrationUpdate(data, "1.3.0");
+        for (const node of result.nodes) {
           node.data.min.should.equal(0);
           node.data.max.should.equal(100);
         }
@@ -100,14 +105,16 @@ describe("Migrations",  () =>
 
     describe("v-1.5 changes", () => {
       it("should have images and paletteItems for nodes", () => {
-        for (const node of this.result.nodes) {
+        const result = migrationUpdate(data, "1.5.0");
+        for (const node of result.nodes) {
           node.data.image.should.not.equal(null);
           node.data.paletteItem.should.not.equal(null);
         }
       });
 
       it("paletteItems should have a uuid", () => {
-        this.result.palette.map((paletteItem) => paletteItem.uuid.should.not.equal(null));
+        const result = migrationUpdate(data, "1.5.0");
+        result.palette.map((paletteItem) => paletteItem.uuid.should.not.equal(null));
       });
     });
 
@@ -118,7 +125,8 @@ describe("Migrations",  () =>
 
     describe("v-1.7 changes", () =>
       it("should have settings for the simulation", () => {
-        this.result.settings.simulation.should.not.equal(undefined);
+        const result = migrationUpdate(data, "1.7.0");
+        result.settings.simulation.should.not.equal(undefined);
       })
     );
         // Removed or changed in 1.8:
@@ -129,41 +137,47 @@ describe("Migrations",  () =>
 
     describe("v-1.8 changes", () =>
       it("should have settings for the simulation", () => {
-        this.result.settings.simulation.duration.should.equal(10);
-        this.result.settings.simulation.stepUnits.should.equal(TimeUnits.defaultUnit);
+        const result = migrationUpdate(data, "1.8.0");
+        result.settings.simulation.duration.should.equal(10);
+        result.settings.simulation.stepUnits.should.equal(TimeUnits.defaultUnit);
       })
     );
 
-//    describe "v-1.9 changes", ->
-//      it "should have speed setting", ->
-//        @result.settings.simulation.speed.should.equal 4
+    // describe "v-1.9 changes", ->
+    //   it "should have speed setting", ->
+    //     @result.settings.simulation.speed.should.equal 4
 
     // Removed in 1.13
     // describe "v-1.10 changes", ->
     //   it "should have newIntegration setting", ->
     //     @result.settings.simulation.newIntegration.should.equal false
 
+    // Removed in 1.25
     describe("v-1.11.0 changes", () =>
       it("should have minigraphs setting", () => {
-        this.result.settings.showMinigraphs.should.equal(false);
+        const result = migrationUpdate(data, "1.11.0");
+        result.settings.showMinigraphs.should.equal(false);
       })
     );
 
     describe("v-1.12.0 changes", () =>
       it("should have frames array", () => {
-        this.result.nodes.map((node) => node.data.frames.should.not.equal(undefined));
+        const result = migrationUpdate(data, "1.12.0");
+        result.nodes.map((node) => node.data.frames.should.not.equal(undefined));
       })
     );
 
     describe("v-1.15.0 changes", () =>
       it("should have link reasoning", () => {
-        this.result.links.map((link) => link.reasoning.should.not.equal(undefined));
+        const result = migrationUpdate(data, "1.15.0");
+        result.links.map((link) => link.reasoning.should.not.equal(undefined));
       })
     );
 
     describe("v-1.16.0 changes", () =>
       it("should not have simulation speed settings", () => {
-        const { speed } = this.result.settings.simulation;
+        const result = migrationUpdate(data, "1.16.0");
+        const { speed } = result.settings.simulation;
         should.equal(speed, undefined);
       })
     );
@@ -178,7 +192,8 @@ describe("Migrations",  () =>
 
     describe("v-1.18.0 changes", () =>
       it("should have link relation type", () => {
-        this.result.links.map((link) => link.relation.type.should.not.equal(undefined));
+        const result = migrationUpdate(data, "1.18.0");
+        result.links.map((link) => link.relation.type.should.not.equal(undefined));
       })
     );
 
@@ -189,8 +204,9 @@ describe("Migrations",  () =>
 
     describe("v-1.22.0 changes", () =>
       it("should have complexity and simulation settings", () => {
-        this.result.settings.simulationType.should.equal(1);
-        this.result.settings.complexity.should.equal(1);
+        const result = migrationUpdate(data, "1.22.0");
+        result.settings.simulationType.should.equal(1);
+        result.settings.complexity.should.equal(1);
       })
     );
 
@@ -200,7 +216,7 @@ describe("Migrations",  () =>
         // containg collector nodes was not corectly migrating the simulationType
         // to time-based. In other words, the wrong value is 1 and the value
         // we want is 2.
-        const brokenExample = v1220data;
+        const brokenExample = _.cloneDeep(v1220data);
         // Pre-test our broken json is our expected version of 1.22.0
         expect(brokenExample.version).to.equal("1.22.0");
         expect(brokenExample.settings.simulationType).to.equal(1); // wrong value!
@@ -213,7 +229,7 @@ describe("Migrations",  () =>
     describe("v-1.24.0 changes", () =>
       describe("a known failing case: missing `combineMethod` in serialization", () => {
         // We don"t seem to be serializing the combinMethod
-        const brokenExample = v1220MissingCombineMethodData;
+        const brokenExample = _.cloneDeep(v1220MissingCombineMethodData);
         // Pre-test our broken json is our expected version of 1.22.0
         expect(brokenExample.version).to.equal("1.22.0");
         // expect to not find any `combineMethod` for our nodes.
@@ -223,6 +239,13 @@ describe("Migrations",  () =>
         migrationUpdate(brokenExample);
         // Now we want all 4 nodes to have combineMethod
         _.each(brokenExample.nodes, n => expect(n.data.combineMethod).to.exist);
+      })
+    );
+
+    describe("v-1.25.0 changes", () =>
+      it("should remove showing minigraphs app setting", () => {
+        const result = migrationUpdate(data, "1.25.0");
+        expect(result.settings.showMinigraphs).to.equal(undefined);
       })
     );
   })
