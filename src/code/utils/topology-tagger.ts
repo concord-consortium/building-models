b12dd0c0bb1a77e3f859350f7b87009661a2e677
Noah Paessel <knowuh@gmail.com>
2019-03-21 14:19:19 -0400

    [DL/NP] -- Simplify and cleanup topology-tagger and test.
    [#164634326]
    
    https://www.pivotaltracker.com/story/show/164634326

--- src/code/utils/topology-tagger.ts
+++ src/code/utils/topology-tagger.ts
@@ -1,31 +1,34 @@
+const _ = require("lodash");
+import { Graph as AnalysisGraph, alg as GraphLibAlg} from "graphlib";
+import { Link } from "../models/link";
+import { Node } from "../models/node";
 
-import { Graph, alg} from "graphlib";
+type sageNodeKey = string;
 
-export interface ILinkStruct {
-  sourceNode: string;
-  targetNode: string;
+export interface ISageLink {
+  sourceNode: sageNodeKey;
+  targetNode: sageNodeKey;
   title: string;
 }
 
-export interface InodeDataStruct {
-  title: string;
-  isAccumulator?: boolean;
+export interface ISageNode {
+  key: sageNodeKey;
 }
 
-export interface INodeStruct {
-  key: string;
-  data: InodeDataStruct;
+export interface ITopology {
+  nodeCount: number;
+  linkCount: number;
+  cycles: number;
+}
+export interface ISageGraph {
+  links: ISageLink[];
+  nodes: ISageNode[];
 }
 
-export interface ISageGraphStruct {
-  links: ILinkStruct[];
-  nodes: INodeStruct[];
-}
-
-export function convertSageModelToGraph(sageModelGraph: ISageGraphStruct) {
-  const graphlibGraph = new Graph();
+export function getAnalysisGraph(sageModelGraph: ISageGraph) {
+  const graphlibGraph = new AnalysisGraph();
   sageModelGraph.nodes.forEach((n) => {
-    graphlibGraph.setNode(n.key, n.data.title);
+    graphlibGraph.setNode(n.key);
   });
   sageModelGraph.links.forEach((l) => {
     graphlibGraph.setEdge(l.sourceNode, l.targetNode, l.title);
@@ -33,10 +36,19 @@ export function convertSageModelToGraph(sageModelGraph: ISageGraphStruct) {
   return graphlibGraph;
 }
 
-export function countNodes(g: Graph) {
+export function countNodes(g: AnalysisGraph) {
   return g.nodeCount();
 }
 
-export function getCycles(g: Graph) {
-  return alg.findCycles(g);
+export function getCycles(g: AnalysisGraph) {
+  return GraphLibAlg.findCycles(g);
+}
+
+
+export function getToplogy(sageModelGraph: ISageGraph) {
+  const g = getAnalysisGraph(sageModelGraph);
+  const nodeCount = g.nodeCount();
+  const linkCount = g.edgeCount();
+  const cycles = GraphLibAlg.findCycles(g);
+  return { nodeCount, linkCount, cycles: cycles.length };
 }
--- test/topology-tagger-test.ts
+++ test/topology-tagger-test.ts
@@ -6,64 +6,51 @@ chai.config.includeStack = true;
 import { v1220data as sageData} from "./serialized-test-data/v-1.22.0";
 import { Graph } from "graphlib";
 import {
-  convertSageModelToGraph,
+  getAnalysisGraph,
   countNodes,
   getCycles,
-  ILinkStruct,
-  INodeStruct,
-  ISageGraphStruct
+  ISageLink,
+  ISageNode,
+  ISageGraph,
+  getToplogy
 } from "../src/code/utils/topology-tagger";
 
 describe("A TopologyTagger", () => {
   beforeEach(() => null);
   afterEach(() => null);
 
-  it("Tests are running", () => {
-    chai.expect(1).to.eql(1);
-  });
-
   it("can convert a Sage model to a graphlib graph", () => {
-    const g = convertSageModelToGraph(sageData);
+    const g = getAnalysisGraph(sageData);
     chai.expect(g).respondTo("nodeCount");
   });
 
   it("can count the number of nodes", () => {
-    const g = convertSageModelToGraph(sageData);
-    g.nodeCount().should.eql(9);
+    chai.expect(getToplogy(sageData).nodeCount).to.eql(9);
   });
 
   it("can count the number of links", () => {
-    const g = convertSageModelToGraph(sageData);
-    g.edgeCount().should.eql(4);
+    chai.expect(getToplogy(sageData).linkCount).to.eql(4);
   });
 
   describe("Topology detection", () => {
     describe("finding cycles", () => {
       it("finds cycles when they exist", () => {
-        const links: ILinkStruct[] = [
+        const links: ISageLink[] = [
           {title: "l1", sourceNode: "a", targetNode: "b"},
           {title: "l2", sourceNode: "b", targetNode: "a"},
         ];
-        const nodes: INodeStruct[] = [
-          {data: {title: "a"}, key: "a"},
-          {data: {title: "b"}, key: "b"}
-        ];
-        const graph: ISageGraphStruct = {nodes, links};
-        const g = convertSageModelToGraph(graph);
-        chai.expect(getCycles(g).length).to.eql(1);
+        const nodes: ISageNode[] = [{key: "a"}, {key: "b"}];
+        const graph: ISageGraph = {nodes, links};
+        chai.expect(getToplogy(graph).cycles).to.eql(1);
       });
 
       it("wont find cycles when they dont exist", () => {
-        const links: ILinkStruct[] = [
+        const links: ISageLink[] = [
           {title: "l1", sourceNode: "a", targetNode: "b"}
         ];
-        const nodes: INodeStruct[] = [
-          {data: {title: "a"}, key: "a"},
-          {data: {title: "b"}, key: "b"}
-        ];
-        const graph: ISageGraphStruct = {nodes, links};
-        const g = convertSageModelToGraph(graph);
-        chai.expect(getCycles(g).length).to.eql(0);
+        const nodes: ISageNode[] = [{key: "a"}, {key: "b"}];
+        const graph: ISageGraph = {nodes, links};
+        chai.expect(getToplogy(graph).cycles).to.eql(0);
       });
     });
   });
