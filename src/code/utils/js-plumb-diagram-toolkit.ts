cdea3db8ffa72f99c6360aba73a8e3d33c8c0dd3
Doug Martin <dmartin@concord.org>
2021-06-11 14:01:10 -0400

    Changed start size on transfer links to 32x32

--- src/code/utils/js-plumb-diagram-toolkit.ts
+++ src/code/utils/js-plumb-diagram-toolkit.ts
@@ -270,9 +270,8 @@ export class DiagramToolkit {
     return () => {
       const svg = $('<svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" viewBox="0 0 24 24" fill="#ececec">')  // ececec
         .html('<path d="M12,1L9,9L1,12L9,15L12,23L15,15L23,12L15,9L12,1Z" />');
-        // .html('<path d="M 1,15 10,20 15,30 20,20 30,15 20,10 15,1 10,10 Z" />');
       return $("<div />")
-        .css({width: 28, height: 26})
+        .css({width: 32, height: 32})
         .append(svg);
     };
   }
